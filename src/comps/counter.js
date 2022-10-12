5e09dbb9d121679212bb0910b4d2814f4b75df01
roentm <93037433+roentm@users.noreply.github.com>
2022-10-12 17:08:14 +0300

    nn

--- src/comps/counter.js
+++ src/comps/counter.js
@@ -1,10 +1,22 @@
+import { useState } from "react"
+
 export default function Counter(props){
+    const [counter,setCounter] = useState(5);
+
+    const changeCounter= ()=>{
+        setCounter(88)
+    }
+    const addCounter=()=>{
+        setCounter(counter+1);
+    }
 
     return(
         <div>
             <h2>
-                Counter: 
+                Counter: {counter}
             </h2>
+            <button className="btn btn-danger mx-2" onClick={()=>{setCounter(0)}}>Reset Counter</button>
+            <button className="btn btn-info mx-2" onClick={addCounter}>Add+1</button>
         </div>
     )
 }
