adfd91e95b37bc2b528da8306a00d7d5551f5833
roentm <93037433+roentm@users.noreply.github.com>
2022-10-12 19:52:51 +0300

    Update appInput.js

--- src/comps/appInput.js
+++ src/comps/appInput.js
@@ -1,9 +1,18 @@
-export default function AppInput(props){
-    return(
-        <div className="col-lg-6 border p-3 my-3">
+import { useRef, useState } from "react";
+
+export default function AppInput(props) {
+    const color = useRef();
+    const [bg, setBG] = useState("");
+
+    const changeColor=()=>{
+        setBG(color.current.value);
+    }
+
+    return (
+        <div style={{ background: bg }} className="col-lg-6 border p-3 my-3">
             <h2>Enter your favorite color:</h2>
-            <input type="text" className="form-control" />
-            <button className="btn btn-info mt-3">Change Color</button>
+            <input ref={color} type="text" className="form-control" />
+            <button className="btn btn-info mt-3" onClick={changeColor}>Change Color</button>
         </div>
     )
 }
