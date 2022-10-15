411c7fadf2ddaa415d50060efbc1e39f099f30a1
roentm <93037433+roentm@users.noreply.github.com>
2022-10-15 14:33:58 +0300

    lesson10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
+import AppHome from './comps/AppHome';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='display-4'>React</h1>
-    </div>
+    <React.Fragment>
+      <AppHome />
+    </React.Fragment>
   );
 }
 
