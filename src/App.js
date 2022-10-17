34802d2d5254f41a55f93968aba0d6df04d9d4c2
roentm <93037433+roentm@users.noreply.github.com>
2022-10-17 12:37:45 +0300

    lesson15

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import AppHome from './comps/appHome';
+import About from './comps/about';
+import Gallery from './comps/gallery';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='display-4'>React</h1>
-    </div>
+    <BrowserRouter>
+      <header className='border p-2'>
+        <h2>header</h2>
+      </header>
+      <Routes>
+        <Route path="/" element={<AppHome/>} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/gallery" element={<Gallery/>}/>
+        <Route path="/**" element={
+          <div className='container-fluid'>
+            <div className='container mx-auto'>
+              <h1 className='text-danger display-2'>
+                Page Not Found, 404
+             </h1>
+            </div>
+          </div>
+        }/>
+      </Routes>
+      <footer>
+        <h3>footer</h3>
+      </footer>
+    </BrowserRouter>
+
   );
 }
 
