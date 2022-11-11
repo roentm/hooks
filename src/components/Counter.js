505b3032459414212d95ffd0ad05a45e518c556d
roentm <93037433+roentm@users.noreply.github.com>
2022-11-11 09:53:49 +0200

    stage2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,21 @@
+import {useState} from 'react'
 import React from 'react'
 import '../counter.css'
 
 export default function Counter() {
+    const [counter,setCounter]= useState(1);
   return (
-    <div className='wrapperCounter'>
-        <h1>0</h1>
-    </div>
+    <React.Fragment>
+        <div className='text-center'>
+            <h1>{counter}</h1>
+        </div>
+        <div className='wrapperCounter'>
+            <button className='me-3 btn btn-info' onClick={()=>{setCounter(counter+1)}}>+</button>
+            <button className='btn btn-danger' onClick={()=>{setCounter(counter-1)}}>-</button>
+        </div>
+        
+    </React.Fragment>
+    
+
   )
 }
