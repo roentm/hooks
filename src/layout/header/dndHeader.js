7609e0fd99f29cf9bea967fb1fb12a745b0860b7
roentm <93037433+roentm@users.noreply.github.com>
2022-11-22 17:57:50 +0200

    base

---
+++ src/layout/header/dndHeader.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './dndHeader.css'
+
+const DndHeader = () => {
+  return (
+    <div className="container-fluid">
+        <div className="container">
+            <div className='row'>
+                <div className="col">
+
+                </div>
+                <div className="col">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DndHeader
