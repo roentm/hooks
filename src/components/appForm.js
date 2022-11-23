a02b82a56162e9dcf2c8d8c34ffff43435b67c1a
roentm <93037433+roentm@users.noreply.github.com>
2022-11-23 14:40:15 +0200

    ok

--- src/components/appForm.js
+++ src/components/appForm.js
@@ -1,49 +1,58 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-
-
-
-const AppForm = () => {
-    const schema = yup.object().shape({
-        firstName: yup.string().required(),
-        characterName: yup.string().required(),
-        email: yup.string().email().required(),
-        birthDate: yup.date().required(),
-        password: yup.string().min(6).max(15).required(),
-        cPassword: yup.string().oneOf([yup.ref("password"), null]).required()
-    })
-    const { register, handleSubmit, errors } = useForm({ resolver: yupResolver(schema) });
-
-    const submitForm = (data) => {
+import React, { useEffect } from 'react'
+import { useForm } from "react-hook-form";
+const FormHook = () => {
+    const { register, getValues, handleSubmit, formState: { errors } } = useForm();
+    const onSub = (data) => {
+        delete data.confirmPassword
         console.log(data);
-    };
+    }
+    const ageCheck=(value) => {
+        const selected = new Date(value).getFullYear();
+        const now = new Date().getFullYear();
+        return now - selected >= 5;
+    }
 
+    console.log(errors)
     return (
-        <div className="form">
-            <div className="title">Sign Up</div>
-            <div className="inputs">
-                <form onSubmit={handleSubmit(submitForm)}>
-                    <input type="text" name="firstName" placeholder="Enter your desired firstName..." ref={register}/>
-                    <small className='text-warning'>{errors.firstName.message}</small>
-                    <input type="text" name="characterName" placeholder="Enter your Character's name..." ref={register}/>
-                    <small className='text-warning' >{errors.characterName.message}</small>
-                    <input type="text" name="email" placeholder="Enter your email..." ref={register}/>
-                    <small className='text-warning'>{errors.email.message}</small>
-                    <input type="date" name="birthDate" placeholder="01/01/1900" ref={register}/>
-                    <small className='text-warning'>{errors.date.message}</small>
-                    <input type="text" name="password" placeholder="Enter your password..." ref={register}/>
-                    <small className='text-warning'>{errors.password.message}</small>
-                    <input type="text" name="cPassword" placeholder="Confirm your password..." ref={register}/>
-                    <small className='text-warning'>{errors.cPassword && "The passwords don't match!"}</small>
-                    <input type="submit" id='submit' />
-                </form>
+        <div className='d-flex flex-column align-items-center '>
+            <h1 className='display-3 text-center text-light'>Login</h1>
 
+            <form onSubmit={handleSubmit(onSub)} className='col-7 col-md-5 col-lg-3 mt-5 form-dark bg-dark text-light'>
+                <div>
+                    <input {...register('userName', { required: { value: true, message: 'first name is requried' }, minLength: { value: 3, message: "first name must be at least 3 characters" } })} className='myInput mt-4' placeholder='Enter your desired Username...'/>
+                    {errors.userName && errors.userName.type == 'required' && <small className='error'>{errors?.userName?.message}</small>}
+                    {errors.userName && errors.userName.type == 'minLength' && <small className='error'>{errors?.userName?.message}</small>}
+                </div>
+                <div>
+                    <input {...register('characterName', { required: true, minLength: 2 })} className='myInput mt-3' placeholder="Enter your character's name..."/>
+                    {errors.characterName && <small className='error'>invalid last name...</small>}
 
-            </div>
+                </div>
+                <div>
+                    <input {...register('email', { required: true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ })} className='myInput mt-3' placeholder="Enter your email..."/>
+                    {errors.email && <small className='error'>Please enter a valid email!</small>}
+                </div>
+                <div>
+                    <input type={'password'}  {...register('password', { required: { value: true, message: 'password is requried' }, minLength: { value: 6, message: "invalid password" } })} className='myInput mt-3' placeholder="Choose a password"/>
+                    {errors.password && errors.password.type == 'required' && <small className='error'>{errors?.password?.message}</small>}
+                    {errors.password && errors.password.type == 'minLength' && <small className='error'>{errors?.password?.message}</small>}
+                </div>
+                <div>
+                    <input type={'password'} {...register('confirmPassword', { required: { value: true, message: 'Confirm password required...' }, validate: { validate:( (value) => value === getValues('password')) } })} className='myInput mt-3' placeholder="Confirm your password"/>
+                    {errors.confirmPassword && errors.confirmPassword.type == 'required' && <small className='error'>{errors?.confirmPassword?.message}</small>}
+                    {errors.confirmPassword && errors.confirmPassword.type == 'validate' && <small className='error'>The passwords don't match!</small>}
+                </div>
+                <div>
+                    <label className='myLabel'>Date of Birth:</label>
+                    <input type={'date'} {...register('dob', {required: {value: true, message: 'Your date of birth is required!'}, validate: ageCheck})} className='myInput'/>
+                    {errors.dob && errors.dob.type=='required' && <small className='error'>{errors?.dob?.message}</small>}
+                    {errors.dob && errors.dob.type== 'validate' && <small className='error'>You must be 18 years old or older to sign up!</small>}
+                </div>
+                <button className='btn btn-info badge p-2 mt-3'>Submit</button>
+            </form>
+
         </div>
     )
 }
 
-export default AppForm
+export default FormHook
