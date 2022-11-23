cdd2151058a481009afb25ea600044c8b97576ec
roentm <bloodythewilliam@gmail.com>
2022-11-23 20:03:44 +0200

    ok

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import AppRoutes from './routers/appRoutes'
+
+const App = () => {
+  return (
+    <>
+    <AppRoutes/>
+    </>
+  )
+}
+
+export default App
---
+++ src/layout/header/header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div className="container-fluid bg-dark">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <Link className="nav-link active" to={'/'}>ReactHookForm</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className="nav-link active" to={'/'}>Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link active" to={'/page1'}>Page 1</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link active" to={'/page2'}>Page 2</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Header
