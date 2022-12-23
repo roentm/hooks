c6f0c4327881a23903ac511aac129ec8cbcc4ee8
roentm <93037433+roentm@users.noreply.github.com>
2022-12-23 15:03:07 +0200

    ok

--- src/consts.js
+++ src/consts.js
@@ -1,1 +1,38 @@
 export const apiURL='https://www.omdbapi.com/?apikey=2be62b1&';
+export const images=[
+    {
+        title:'The Master',
+        img:'../public/imgs/1.jpg',
+        omdbID:'tt1560747'
+    },
+    {
+        title:'In the Mood for Love',
+        img:'../public/imgs/2.jpg',
+        omdbID:'tt0118694'
+    },
+    {
+        title:'Mulholland Drive',
+        img:'../public/imgs/3.jpg',
+        omdbID:'tt0166924'
+    },
+    {
+        title:'There Will be Blood',
+        img:'../public/imgs/4.jpg',
+        omdbID:'tt0469494'
+    },
+    {
+        title:'The Revenant',
+        img:'../public/imgs/5.jpg',
+        omdbID:'tt1663202'
+    },
+    {
+        title:'The Lord of the Rings: The Fellowship of the Ring',
+        img:'../public/imgs/6.jpg',
+        omdbID:'tt0120737'
+    },
+    {
+        title:'Phantom Thread',
+        img:'../public/imgs/7.jpg',
+        omdbID:'tt5776858'
+    }
+]
