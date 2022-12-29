193e1a8679539a9d95514371027dcb45fc1c6ddd
roentm <93037433+roentm@users.noreply.github.com>
2022-12-29 14:40:52 +0200

    Update Slider.jsx

--- src/layout/headerSlider/Slider.jsx
+++ src/layout/headerSlider/Slider.jsx
@@ -10,14 +10,46 @@ const Slider = ({images}) => {
         backgroundPosition: "center",
         backgroundSize: "cover",
         backgroundImage: `url(${images[curIndex].img})`
-    }
+    };
     const sliderStyle={
         height: "100%",
         position: "relative"
 
+    };
+    const leftArrowStyles={
+      position: "absolute",
+      top: "50%",
+      transform: 'translate(0, -50%)',
+      left: "32px",
+      fontSize: "55px",
+      color: "#fff",
+      zIndex: 1,
+      cursor: "pointer",
+    };
+    const rightArrowStyles={
+      position: "absolute",
+      top: "50%",
+      transform: 'translate(0, -50%)',
+      right: "32px",
+      fontSize: "55px",
+      color: "#fff",
+      zIndex: 1,
+      cursor: "pointer",
+    };
+    const goBack=()=>{
+      const isFirstSlider= curIndex ===0;
+      const newIndext=isFirstSlider?images.length-1:curIndex-1;
+      setCurIndex(newIndext)
     }
+    const goForward=()=>{
+      const islastSlider= curIndex===images.length-1;
+      const newIndext=islastSlider?0:curIndex+1;
+      setCurIndex(newIndext);
+    };
   return (
     <div style={sliderStyle}>
+        <div style={leftArrowStyles} onClick={goBack} title='Previous Item'>❰</div>
+        <div style={rightArrowStyles} onClick={goForward} title='Next Item'>❱</div>
         <div style={slidesStyle}>
         </div>
     </div>
