5612a522de0866eef2d24e8116a0ed9c4f5be71f
roentm <93037433+roentm@users.noreply.github.com>
2022-12-29 14:12:52 +0200

    Update layout.jsx

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import {Fragment} from 'react'
 import {Outlet} from 'react-router-dom'
 import Footer from './footer/footer'
-import Header from './header/header'
 import HeaderSlider from './headerSlider/HeaderSlider'
 
 const Layout = () => {
   return (
     <>
         <HeaderSlider/>
-        {/* <Header/> */}
         <Outlet/>
         <Footer/>
     </>
